c61e83490ae8fb0a6220dfc19bf29afe8c1187ee
HarshadUgale5 <hbugaleofficial@gmail.com>
2022-04-09 19:59:43 +0530

    added memory_Access

---
+++ code/Memory_Access.v
@@ -0,0 +1,14 @@
+// Authors : 
+//	Harshad Bhausaheb Ugale
+//	Mahesh Shahaji Patil
+
+// Description :
+//	This module reads memory and gived data.
+
+
+module Memory_Access
+(
+	input resetn,flush,
+	input [36:0] MemAccessInData,
+	
+);
