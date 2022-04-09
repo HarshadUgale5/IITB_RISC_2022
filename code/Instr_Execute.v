93a860fe58fbee75ef15106c373491a12dae7f6e
Mahi36 <39255054+Mahi36@users.noreply.github.com>
2022-04-09 20:01:55 +0530

    New.

--- code/Instr_Execute.v
+++ code/Instr_Execute.v
@@ -3,107 +3,46 @@
 //	Mahesh Shahaji Patil
 
 // Description :
-//	This module executes the instruction.
-
-module Instr_Execute
+//	This module initially loads instructions from 
+//	 program_instr.hex file and stores them
+//	 into Instruction_mem.
+//	Then, reads instruction from instr_mem using PC.
+module Instr_Fetch
 (
 	input clk,resetn,flush,
-	input [41:0] ExInData,
-	output wire update,
-	output wire [36:0] OutToMA
+	output wire [15:0] Instruction
+	//output reg reg_enable
 );
-	//	flags[0] -> C and flags[1] -> Z
-	reg [1:0] flags;
-	wire [1:0] old_flags;
-	reg update_w;
-	reg [36:0] Out;
+	reg  [15:0] PC;
+	reg [3:0]  instr_mem_addr;
+	reg  [15:0] instr;	
+	reg  [15:0] instr_mem [0:15];
 
-	kbitwidthReg #(2) Flag_reg (.Din(flags),.clk(clk),.resetn(resetn), .ld(1'b1),.Qout(old_flags));	
+	initial
+		begin
+		$readmemb("program_instr.hex",instr_mem); 
+		end
 
-	always @(ExInData,resetn,flush)
-		if(!resetn || flush)
+	always @ (posedge clk or negedge resetn)
+		begin
+		if(!resetn)
 			begin
-			Out = 37'd0;
-			flags = 2'd0;
-			update_w = 0;
+			// if resetn = '0' clear all
+			instr = 16'd0;
+			PC = 16'd0;
+			//reg_enable = 0;
 			end
+		else if(flush)
+			instr = 16'd0; 
 		else
 			begin
-			case(ExInData[6:2])
-				5'b00001 :	// ADD
-					begin
-					{flags[0],Out[20:5]} = {1'b0,ExInData[41:26]} + {1'b0,ExInData[25:10]};
-					update_w = 1;
-					flags[1] = ~|Out[20:5];
-					end
-				5'b00010 :	// ADC
-					if(old_flags[0])
-						begin
-						{flags[0],Out[20:5]} = {1'b0,ExInData[41:26]} + {1'b0,ExInData[25:10]};
-						update_w = 1;
-						flags[1] = ~|Out[20:5];
-						end
-					else
-						update_w = 0;
-				5'b00011 :	// ADZ
-					if(old_flags[1])
-						begin	
-						{flags[0],Out[20:5]} = {1'b0,ExInData[41:26]} + {1'b0,ExInData[25:10]};
-						update_w = 1;
-						flags[1] = ~|Out[20:5];
-						end
-					else
-						update_w = 0;
-				5'b00100 :	// ADL
-					begin
-					{flags[0],Out[20:5]} = {1'b0,ExInData[41:26]} + {1'b0,(ExInData[25:10] << 1)};
-					update_w = 1;
-					flags[1] = ~|Out[20:5];
-					end
-				5'b00101 :	// NDU
-					begin
-					Out[20:5] = ~(ExInData[41:26] & ExInData[25:10]) ;
-					update_w = 1;
-					flags[1] = ~|Out[20:5];
-					end
-				5'b00110 :	// NDC
-					if(old_flags[0])
-						begin
-						Out[20:5] = ~(ExInData[41:26] & ExInData[25:10]);
-						update_w = 1;
-						flags[1] = ~|Out[20:5];
-						end
-					else
-						update_w = 0;
-				5'b00111 :	// NDZ
-					if(old_flags[1])
-						begin	
-						Out[20:5] = ~(ExInData[41:26] & ExInData[25:10]);
-						update_w = 1;
-						flags[1] = ~|Out[20:5];
-						end
-					else
-						update_w = 0;
-				5'b01000 :	// LHI
-					begin
-					Out[20:5] = {ExInData[18:10],7'd0};
-					update_w = 1;
-					end
-				default :	// NOP
-					update_w = 0;
-			endcase
+			// else fetch new instr.
+			//reg_enable = 0;
+			instr_mem_addr = PC[3:0];
+			instr = instr_mem[instr_mem_addr];
+			PC = PC + 1'b1;
+			//reg_enable = 1;
 			end
-
-	assign update = update_w;
-	assign OutToMA = {ExInData[41:26],Out[20:5],ExInData[9:7],ExInData[1:0]};
-
+		end
+	assign Instruction = instr;
 endmodule
-
-
-// 000000000000001000000000000000010110000111 // ADD R6,1,2
-// 111111111111111100000000000000010110000111 // ADD R6,2^16-1,1
-// 000000000000001000000000000000010110001011 // ADC R6,
-// 000000000000001000000000000000010110010111 // NDU R6,1,2
-// 000000000000001000000000000000010110011011 // NDC R6,1,2
-// 000000000000001000000000000000010110011111 // NDZ R6,1,2
-// 000000000000001000000000000000010110100001 // LHI R6,1<<10
